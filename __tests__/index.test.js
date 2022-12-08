03789925c7bd7eaa68655ad14ce3bb9c461bce69
Павел Кузнецов <pavel_iv37@mail.ru>
2022-12-08 11:57:42 +0300

    step 4 add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,13 @@
+import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import genDiff from '../src/index.js';
-import expected from '../__fixtures__/expected.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
+const readFile = (filename) => readFileSync((getFixturePath(filename)), 'utf8');
+const expected = readFile('expected.txt');
 test('gendiff', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expected);
 });

029abd9d049cf53ad9b0aa26beb87c83609ff1de
Павел Кузнецов <pavel_iv37@mail.ru>
2022-12-19 14:21:56 +0300

    step 6.3.1

--- src/stylish.js
+++ src/stylish.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
 const makeIndent = (depth, spacesCount = 4) => ' '.repeat(spacesCount * depth - 2);
-/*const stringify = (value, depth = 1) => {
+
+const stringify = (value, depth = 1) => {
   
   if (typeof value !== 'object' || value === null) {
     return String(value);
@@ -46,57 +47,6 @@ const iter = (value, depth) => {
 
   return iter(tree, 1)
   
-};*/
-
-
-const stringify = (value) => {
-  const iter = (carrentValue, depth) => {
-    if (typeof carrentValue !== 'object' || carrentValue === null) {
-      return String(carrentValue);
-    }
-
-    const massValue = Object.entries(carrentValue);
-    const lines = massValue.map(([key, val]) => {
-        return `${makeIndent(depth )}${key}: ${iter(val, depth + 1)}`;
-      })
-      const result = ['{', ...lines, `${makeIndent(depth + 1 )}}`].join('\n');
-      
-      return result;
-
-  }
-  return iter(value, 1);
-};
-
-const getTree = (tree, depth = 1) => {
- // console.log('tree: ', tree)
-    
-     const lines = tree.map((obj) => {
-      //console.log('obj: ', obj)
-     
-      const {key, type, children, value, valueAfter, valueBefore} = obj;
-
-        //console.log('key: ', key)
-
-      //  console.log('key: ', key)
-        //console.log('type: ', type)
-        switch (type) {
-          case 'added':
-            return `${makeIndent(depth + 1)}+ ${key}: ${stringify(value, depth)}`; 
-          case 'deleted':
-            return `${makeIndent(depth + 1 )}- ${key}: ${stringify(value, depth)}`; 
-          case 'nested' :
-            return `${makeIndent(depth)}  ${key}: ${(stringify(children, depth + 1))}\n${makeIndent(depth)}`;
-          case  'changed':
-            return `${makeIndent(depth + 1)}- ${key}: ${stringify(valueBefore, depth)}\n${makeIndent(depth + 1)}+ ${key}: ${stringify(valueAfter, depth)}`;
-          case 'unchanged':
-           return `${makeIndent(depth + 1)}  ${key}: ${stringify(value, depth)}`; 
-          default :
-          return 'unknown type'
-        }
-      });
-      return ['{', ...lines, `${makeIndent(depth)}}`].join('\n');
-      
-    
 };
 
   export default getTree;
